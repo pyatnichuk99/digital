d1d72d1bb5dbad6adc53ade1de053ee08e248a5d
pyatnichuk99 <49722510+pyatnichuk99@users.noreply.github.com>
2019-10-10 15:21:12 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
+$(document).ready(function () {
 
-let menu_mobile = document.getElementById("menu__mobile");
-console.log(menu_mobile);
-// menu_mobile.addEventListener
+    $('.first-button').on('click', function () {
+      $('.animated-icon2').removeClass('open');
+    });
+    $('.second-button').on('click', function () {
+      $('.animated-icon2').toggleClass('open');
+      $('.menu-nav').toggleClass('open__class');
+      $('.social').toggleClass('open__class');
+      // $('.heading').css("display","flex");
+      // $('.nav__search').toggleClass('open__class');
+    });
+    // $('.third-button').on('click', function () {
+  
+    //   $('.animated-icon3').toggleClass('open');
+    // });
+  });
+
+  var btn = $('#button__down');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+  
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '400');
+  });
